62031dd0b80ae2716719605f5d8ce4dfef505c72
Brian Lee <knets22@gmail.com>
2025-11-04 22:20:39 +0900

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ export default function Footer() {
   return (
     <footer className="bg-gray-800 text-gray-200 text-center py-6 mt-8">
       <p>© 2025 Plannerscom. All rights reserved.</p>
-      <p>주소: 서울특별시 서초구 서초동 1444-21 돈일빌딩 2층 210호</p>
-      <p>전화: 02-1234-5678</p>
+      <p>주소: 서울 서초구 서초중앙로2길 35 (서초동, 돈일빌딩) 203호</p>
+      <p>전화: 02-6952-0140</p>
     </footer>
   );
 }
