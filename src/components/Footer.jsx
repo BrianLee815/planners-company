827ec42a2ddc33d9c39d20d3f812bb642d7cd616
Brian Lee <knets22@gmail.com>
2025-11-05 23:13:46 +0900

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-800 text-gray-400 text-center py-6 mt-8">
       <p>© 2025 (주)플래너스컴퍼니 All rights reserved.</p>
-      <p>주소: 서울 서초구 서초중앙로2길 35 (서초동, 돈일빌딩) 203호</p>
+      <p>주소: 서울 서초구 서초중앙로2길 35 (서초동, 돈일빌딩) 210호</p>
       <p>전화: 02-6952-0140</p>
     </footer>
   );
