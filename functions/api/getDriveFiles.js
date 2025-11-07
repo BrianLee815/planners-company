4e29c70e312c6fbef4f353f2df1d59ace09b7238
Brian Lee <knets22@gmail.com>
2025-11-07 13:01:00 +0900

    fix

--- functions/api/getDriveFiles.js
+++ functions/api/getDriveFiles.js
@@ -1,18 +1,15 @@
-export async function onRequestGet() {
-
-  console.log("GOOGLE_API_KEY:", process.env.GOOGLE_API_KEY);
+export async function onRequestGet({ env }) {
   const folderId = "17sWL-j-7bl0vqBr60a5tiIn865mSkj_4";
-  const apiKey = process.env.GOOGLE_API_KEY;
+  const apiKey = env.GOOGLE_API_KEY; // ✅ 수정됨!
 
-  const url = `https://www.googleapis.com/drive/v3/files?q='${folderId}'+in+parents&key=${apiKey}&fields=files(id,name)`;
+  const url = `https://www.googleapis.com/drive/v3/files?q='${folderId}'+in+parents&key=${apiKey}&fields=files(id,name,mimeType)`;
 
   const res = await fetch(url);
   const data = await res.json();
 
-  const images = data.files.map(file => ({
-    id: file.id,
-    url: `https://drive.google.com/uc?export=view&id=${file.id}`
-  }));
+  const images = data.files
+    .filter(file => file.mimeType.includes("image"))
+    .map(file => `https://drive.google.com/uc?export=view&id=${file.id}`);
 
   return new Response(JSON.stringify(images), {
     headers: { "Content-Type": "application/json" },
