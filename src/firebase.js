0f374d812ff408fc4216bbab14eea2e5f408884e
Brian Lee <knets22@gmail.com>
2025-11-07 21:20:07 +0900

    fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
-// firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARZ-COer1ChuZLkvtcCj9G2vRA4LG0qco",
-  authDomain: "planners-company-df3d4.firebaseapp.com",
-  projectId: "planners-company-df3d4",
-  storageBucket: "planners-company-df3d4.firebasestorage.app",
-  messagingSenderId: "49402589489",
-  appId: "1:49402589489:web:e1c388db28cc94762e0010",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
